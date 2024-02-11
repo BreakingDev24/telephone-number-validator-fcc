05e1f72b6122d112dbc48a3d4f01e7dd407a713b
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2024-02-11 20:20:01 +0100

    set variables

---
+++ script.js
@@ -0,0 +1,7 @@
+//variables
+
+const userInput = document.getElementById('user-input')
+const resultsDiv = document.getElementById('results-div')
+const checkBtn = document.getElementById('check-btn')
+const clearBtn = document.getElementById('clear-btn')
+
